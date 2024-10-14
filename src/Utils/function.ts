2865ad8418a68d752f71a5f5f0b6f53e7d4d078d
kerllouskhairy987 <kyrlskhyry@80gmail.com>
2024-10-14 23:04:10 +0300

    jsdocs comment for function

--- src/Utils/function.ts
+++ src/Utils/function.ts
@@ -1,3 +1,10 @@
+/**
+ * Slices a given text to a specified maximum length and appends an ellipsis ("...") if the text exceeds that length.
+ * 
+ * @param {string} txt - The input text to be sliced.
+ * @param {number} [max=60] - The maximum length of the text before it is truncated. Defaults to 60 if not provided.
+ * @returns {string} - The sliced text, with an ellipsis appended if the original text exceeds the specified length.
+ */
 export function txtSlicer(txt: string, max: number = 60) {
     if(txt.length >= max) return `${txt.slice(0, max)} ...`;
     return txt;
