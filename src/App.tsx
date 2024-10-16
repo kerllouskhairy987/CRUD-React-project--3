1553748e2039b5a819e2d8e31609f2f710c6f63d
kerllouskhairy987 <kyrlskhyry@80gmail.com>
2024-10-16 12:29:05 +0300

    Advanced Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import ProductCard from "./components/ProductCard";
+import Modal from "./components/ui/Modal";
 import { productList } from "./data";
+import Button from "./components/ui/Button";
 
 
 function App() {
+
+  // ** States
+  let [isOpen, setIsOpen] = useState(false)
+
+  // ** Handler
+  function open() {
+    setIsOpen(true)
+  }
+
+  function close() {
+    setIsOpen(false)
+  }
+
   // ** Renders
   const renderProductList = productList.map((item) => <ProductCard key={item.id} product={item} />)
 
   return (
     <main className="container">
+      <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={open}>ADD TASK</Button>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-4 gap-4">
         {renderProductList}
       </div>
+
+      <Modal isOpen={isOpen} close={close} title="ADD A NEW PRODUCT" >
+        <div className="flex items-center space-x-4">
+          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+          <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+        </div>
+      </Modal>
     </main>
   )
 }
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,45 @@
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { ReactNode } from 'react';
+
+
+interface IProps {
+    isOpen: boolean;
+    close: () => void;
+    children: ReactNode;
+    title?: string;
+    description?: string;
+}
+
+export default function Modal({ isOpen, close, children, title, description }: IProps) {
+    return (
+        <>
+            <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close} __demoMode>
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-black/15">
+                    <div className="flex min-h-full items-center justify-center p-4 ">
+                        <DialogPanel
+                            transition
+                            className="w-full max-w-md rounded-xl p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 bg-white border-2 shadow-md"
+                        >
+
+                            {title && <DialogTitle as="h3" className="text-base/7 font-medium">
+                                {title}
+                            </DialogTitle>}
+
+                            {description && <p className="mt-2 text-sm/6 text-white/50">{description}</p>}
+
+                            <div className="mt-4">
+                                {children}
+                                {/* <Button
+                                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 "
+                                    onClick={close}
+                                >
+                                    Got it, thanks!
+                                </Button> */}
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+        </>
+    )
+}
