1966616b19ef493754e685dce401972904e6c456
kerllouskhairy987 <kyrlskhyry@80gmail.com>
2024-10-18 18:30:27 +0300

    onCancel Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import ProductCard from "./components/ProductCard";
 import Modal from "./components/ui/Modal";
 import { formInputsList, productList } from "./data";
@@ -9,9 +9,7 @@ import { IProduct } from "./Interfaces";
 
 function App() {
 
-  // ** States
-  const [isOpen, setIsOpen] = useState(false);
-  const [product, setProduct] = useState<IProduct>({
+  const defaultProductObj = {  // to make the input is empty
     title: "",
     description: "",
     imageURL: "",
@@ -21,17 +19,16 @@ function App() {
       name: "",
       imageURL: "",
     }
-  })
-
-
-  // ** Handler
-  function open() {
-    setIsOpen(true)
   }
 
-  function close() {
-    setIsOpen(false)
-  }
+  // ** States ** //
+  const [isOpen, setIsOpen] = useState(false);
+  const [product, setProduct] = useState<IProduct>(defaultProductObj)
+
+  // ** Handler ** //
+  const open = () => setIsOpen(true)
+
+  const close = () => setIsOpen(false)
 
   const onChagneHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
@@ -39,9 +36,20 @@ function App() {
       ...product,
       [name]: value,
     })
+  };
+
+  const onSubmitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log(product);
   }
 
-  // ** Renders
+  // TO close the modle and empty the inputs
+  const onCancel = () => {
+    setProduct(defaultProductObj);
+    close();  // OR if you have many conditions make the state = false ==> setIsOpen(false)
+  }
+
+  // ** Renders ** //
   const renderProductList = productList.map((item) => <ProductCard key={item.id} product={item} />)
 
   const renderFormInputList = formInputsList.map((input) =>
@@ -66,11 +74,11 @@ function App() {
       </div>
 
       <Modal isOpen={isOpen} close={close} title="ADD A NEW PRODUCT" >
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={onSubmitHandler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-            <Button className="bg-gray-400 hover:bg-gray-500">Cancel</Button>
+            <Button className="bg-gray-400 hover:bg-gray-500" onClick={onCancel}>Cancel</Button>
           </div>
         </form>
       </Modal>
